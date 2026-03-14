33b24d45cfef770fa6739f4ff70c42c7f9518c94
dixitprathamesh4579 <dixitprathamesh4579@gmail.com>
2026-03-14 14:55:27 +0530

    Update pattern68.java

--- pattern68.java
+++ pattern68.java
@@ -1,1 +1,18 @@
 class pattern68
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		int count=1;
+		for(int i=1;i<=n;i++)
+		{
+			int c=count;
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(c+"\t");
+				c+=j+1;
+			}
+			count+=i;
+			System.out.println();		}
+	}
+}
