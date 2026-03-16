3cb6f4587e049a7c19139949f2ea81abf9a4d4aa
dixitprathamesh4579 <dixitprathamesh4579@gmail.com>
2026-03-16 21:59:45 +0530

    Refactor pattern80 class to change output pattern

--- pattern80.java
+++ pattern80.java
@@ -2,26 +2,22 @@ class pattern80
 {
 	public static void main(String [] args)
 	{
-		int n = 5;
-		int count = ((n*(n-1)/2)+1);
-
-		for(int i = 1; i <= n; i++)
+		int n=5;
+		int count=((n*(n-1)/2)+1);
+		for(int i=n;i>=1;i--)
 		{
-			int c = count;
-
-			for(int j = i; j < n; j++)
+			int c=count;
+			for(int j=i;j>1;j--)
 			{
 				System.out.print("\t");
 			}
-
-			for(int j = 1; j <= i; j++)
+			for(int j=i;j<=n;j++)
 			{
-				System.out.print(c + "\t");
-				c += (n - j);
+				System.out.print(c+"\t");
+				c+=j+1;
 			}
-
-			System.out.println();
-			count -= (n - i);
-		}
+				count-=i-1;
+				System.out.println();
+				}
 	}
 }
