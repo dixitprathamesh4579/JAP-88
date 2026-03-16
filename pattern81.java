42e986bdeb6cb9607b847d4f619c69ed2fd0c872
dixitprathamesh4579 <dixitprathamesh4579@gmail.com>
2026-03-16 22:02:08 +0530

    upper triangle gap counter patterns  completed

---
+++ pattern81.java
@@ -0,0 +1,23 @@
+class pattern81
+{
+public static void main(String args[])
+{
+	int n=5;
+	int count=(n*(n+1)/2);
+	for(int i=n;i>=1;i--)
+	{
+		int c=count;
+		for(int j=i;j>1;j--)
+		{
+			System.out.print("\t");
+		}
+		for(int j=i;j<=n;j++)
+		{
+			System.out.print(c+"\t");
+			c+=j;
+		}
+		count-=i;
+		System.out.println();
+	}
+}
+}
