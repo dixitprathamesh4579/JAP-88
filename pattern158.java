b034c2e8e444c8a22912cde02506ec8ee8b3bf82
Prathamesh Dixit <dixitprathamesh4579@gmail.com>
2026-04-21 11:32:46 +0530

    pattern

---
+++ pattern158.java
@@ -0,0 +1,18 @@
+class pattern158
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{ 
+		      if(i%2!=0)
+				  System.out.print((char)(i+64));
+			  else
+				  System.out.print((char)(i+96));
+			}
+			System.out.println();
+		}
+	}
+}
